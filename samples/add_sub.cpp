5a64f358d1455d2ff9723aa2654dd35f83cc32eb
Andrey Sv <andrey.gr.sv@gmail.com>
2020-08-20 00:09:44 +0300

    [BDIG] Text formatting

--- samples/add_sub.cpp
+++ samples/add_sub.cpp
@@ -1,31 +1,30 @@
 #include "../bdig.hpp"
 #include <iostream>
 
-int main()
-{
-    ::sag::bdig<20,100> value1 = 200, value2 = 200, result;
-    result = value1 + value2;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result -= "0.1111111111";
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result += "0.1111111111";
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result -= "0.00000000000000000000000000000000000000000000000001";
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result += "0.00000000000000000000000000000000000000000000000001";
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result += 555555555;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result -= 555555555;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result = 100 + value1 - 300 - value2;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result -= 200;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result -= -200;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result += 200;
-    ::std::cout << ::std::string(result) << ::std::endl;
-    result += -200;
-    ::std::cout << ::std::string(result) << ::std::endl;
+int main() {
+  ::sag::bdig<20, 100> value1 = 200, value2 = 200, result;
+  result = value1 + value2;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result -= "0.1111111111";
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result += "0.1111111111";
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result -= "0.00000000000000000000000000000000000000000000000001";
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result += "0.00000000000000000000000000000000000000000000000001";
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result += 555555555;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result -= 555555555;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result = 100 + value1 - 300 - value2;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result -= 200;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result -= -200;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result += 200;
+  ::std::cout << ::std::string(result) << ::std::endl;
+  result += -200;
+  ::std::cout << ::std::string(result) << ::std::endl;
 }
